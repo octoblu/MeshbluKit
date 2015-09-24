acb58b77dd68470bf23841b093861d7f3011e0d2
Sqrt of Saturn <sqrt@octoblu.com>
2015-09-24 09:07:25 -0700

    1.2.16

---
+++ Pod/Classes/MeshbluHttpRequester.swift
@@ -0,0 +1,132 @@
+import Foundation
+import Alamofire
+
+public class MeshbluHttpRequester {
+  let host : String
+  let port : Int
+
+  public init(host : String, port : Int){
+    self.host = host
+    self.port = port
+  }
+
+  public convenience init(){
+    self.init(host: "meshblu.octoblu.com", port: 443)
+  }
+
+  public convenience init(meshbluConfig: [String: AnyObject]){
+    self.init(host: "example", port: 1)
+  }
+
+  private func getManager() -> Manager {
+    return Alamofire.Manager.sharedInstance
+  }
+
+  public func setDefaultHeaders(uuid : String, token : String) {
+    var defaultHeaders = getManager().session.configuration.HTTPAdditionalHeaders ?? [:]
+    defaultHeaders["meshblu_auth_uuid"] = uuid
+    defaultHeaders["meshblu_auth_token"] = token
+
+    getManager().session.configuration.HTTPAdditionalHeaders = defaultHeaders
+  }
+
+  public func delete(path : String, parameters : [String: AnyObject], handler: (Result<JSON, NSError>) -> ()){
+    let urlComponent = NSURLComponents()
+    urlComponent.port = self.port
+    urlComponent.host = self.host
+    urlComponent.scheme = urlComponent.port == 443 ? "https" : "http"
+    urlComponent.path = path
+    let url = urlComponent.string!
+
+    getManager().request(.DELETE, url, parameters: parameters)
+      .responseJSON { (request, response, data, error) in
+        if error != nil || data == nil {
+          let error = NSError(domain: "com.octoblu.meshblu", code: 500, userInfo: [:])
+          handler(Result(error: error))
+        }else{
+          let json = JSON(data!)
+          handler(Result(value: json))
+        }
+    }
+  }
+
+  public func get(path : String, parameters : [String: AnyObject], handler: (Result<JSON, NSError>) -> ()){
+    let urlComponent = NSURLComponents()
+    urlComponent.port = self.port
+    urlComponent.host = self.host
+    urlComponent.scheme = urlComponent.port == 443 ? "https" : "http"
+    urlComponent.path = path
+    let url = urlComponent.string!
+
+    getManager().request(.GET, url, parameters: parameters)
+      .responseJSON { (request, response, data, error) in
+        if error != nil || data == nil {
+          let error = NSError(domain: "com.octoblu.meshblu", code: 500, userInfo: [:])
+          handler(Result(error: error))
+        }else{
+          let json = JSON(data!)
+          handler(Result(value: json))
+        }
+    }
+  }
+
+  public func patch(path : String, parameters : [String: AnyObject], handler: (Result<JSON, NSError>) -> ()){
+    let urlComponent = NSURLComponents()
+    urlComponent.port = self.port
+    urlComponent.host = self.host
+    urlComponent.scheme = urlComponent.port == 443 ? "https" : "http"
+    urlComponent.path = path
+    let url = urlComponent.string!
+
+    getManager().request(.PATCH, url, parameters: parameters)
+      .responseJSON { (request, response, data, error) in
+        if error != nil || data == nil {
+          let error = NSError(domain: "com.octoblu.meshblu", code: 500, userInfo: [:])
+          handler(Result(error: error))
+        }else{
+          let json = JSON(data!)
+          handler(Result(value: json))
+        }
+    }
+  }
+
+  public func post(path : String, parameters : [String: AnyObject], handler: (Result<JSON, NSError>) -> ()){
+    let urlComponent = NSURLComponents()
+    urlComponent.port = self.port
+    urlComponent.host = self.host
+    urlComponent.scheme = urlComponent.port == 443 ? "https" : "http"
+    urlComponent.path = path
+    let url = urlComponent.string!
+
+    getManager().request(.POST, url, parameters: parameters)
+      .responseJSON { (request, response, data, error) in
+        if error != nil || data == nil {
+          let error = NSError(domain: "com.octoblu.meshblu", code: 500, userInfo: [:])
+          handler(Result(error: error))
+        }else{
+          let json = JSON(data!)
+          handler(Result(value: json))
+        }
+    }
+  }
+
+  public func put(path : String, parameters : [String: AnyObject], handler: (Result<JSON, NSError>) -> ()){
+    let urlComponent = NSURLComponents()
+    urlComponent.port = self.port
+    urlComponent.host = self.host
+    urlComponent.scheme = urlComponent.port == 443 ? "https" : "http"
+    urlComponent.path = path
+    let url = urlComponent.string!
+
+    getManager().request(.PUT, url, parameters: parameters)
+      .responseJSON { (request, response, data, error) in
+        if error != nil || data == nil {
+          let error = NSError(domain: "com.octoblu.meshblu", code: 500, userInfo: [:])
+          handler(Result(error: error))
+        }else{
+          let json = JSON(data!)
+          handler(Result(value: json))
+        }
+    }
+  }
+}
